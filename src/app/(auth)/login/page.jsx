1756b0062fb1219b46fbea3f553c3d29494be98f
sisu-01 <tesv010615@naver.com>
2025-01-07 10:41:28 +0900

    next-auth

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,21 @@
+import { auth, signIn } from "@/lib/auth";
+
+const LoginPage = async () => {
+
+  const session = await auth();
+  console.log(session);
+  
+  const handleGithubLogin = async () => {
+    "use server"
+    await signIn("github");
+  }
+  return (
+    <div>
+      <form action={handleGithubLogin}>
+        <button>Login with Github</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/lib/auth.js
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+import GitHubProvider from "next-auth/providers/github";
+export const { handlers: {GET, POST}, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    })
+  ],
+})
