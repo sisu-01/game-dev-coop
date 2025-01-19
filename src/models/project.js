12cb3b90c6af9ceca65818929bb05955e6ab2a1a
sisu-01 <tesv010615@naver.com>
2025-01-19 10:57:01 +0900

    mongodb 스럽게

--- src/models/project.js
+++ src/models/project.js
@@ -7,7 +7,10 @@ const ProjectSchema = new mongoose.Schema({
   endAt: { type: Date },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserProject', select: 'userId' }]
+  users: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // User 컬렉션의 참조
+  }],
 });
 
 export default mongoose.models.Project || mongoose.model("Project", ProjectSchema);
--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,11 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   image: { type: String, },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now },
+  projects: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Project", // Project 컬렉션의 참조
+  }],
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
