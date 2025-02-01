09fb1aa82e8fcaa0895f9d642ff5df7e95b16a83
sisu-01 <tesv010615@naver.com>
2025-02-01 17:50:42 +0900

    대시보드 기초 레이아웃

--- src/app/(with-layout)/[projectId]/dashboard/page.jsx
+++ src/app/(with-layout)/[projectId]/dashboard/page.jsx
@@ -1,8 +1,19 @@
 "use client";
 
+import styles from "./dashboard.module.css";
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        대시 보드
+      </div>
+      <div className={styles.content}>
+        <div className={styles.wrapper}>
+          2025<br/>2025<br/>2025<br/>2025<br/>2025<br/>2025<br/>2025<br/>2025<br/>2025<br/>2025<br/>
+        </div>
+      </div>
+    </div>
   );
 }
 
