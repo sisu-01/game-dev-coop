a4e6893389b233586ac6f2f0b50698de83fc2b06
sisu-01 <tesv010615@naver.com>
2025-02-02 14:16:24 +0900

    칸반보드 css

--- src/app/(with-layout)/[projectId]/kanbanboard/page.jsx
+++ src/app/(with-layout)/[projectId]/kanbanboard/page.jsx
@@ -1,8 +1,26 @@
 "use client";
+import styles from "./kanbanboard.module.css";
 
 const KanbanBoard = () => {
   return (
-    <div>KanbanBoard</div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        칸반 보드
+      </div>
+      <div className={styles.content}>
+        <div className={styles.wrapper}>
+          <div>
+            진행 예정<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z<br/>z
+          </div>
+          <div>
+            진행 중
+          </div>
+          <div>
+            진행 완료
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
