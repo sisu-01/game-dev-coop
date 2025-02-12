c3ada56873c35f8f3907a399e50b236a977e3b17
sisu-01 <tesv010615@naver.com>
2025-02-12 22:20:20 +0900

    프로젝트 삭제시 task도

--- src/app/api/project/delete/route.js
+++ src/app/api/project/delete/route.js
@@ -2,6 +2,7 @@ import { connectToDb } from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 import UserProject from "@/models/user_project";
 import Project from "@/models/project";
+import Task from "@/models/task";
 
 export const DELETE = async (request) => {
   if (request.method === "DELETE") {
@@ -17,6 +18,7 @@ export const DELETE = async (request) => {
         await Project.findByIdAndDelete({ _id: projectId });
         //await UserProject.findOneAndDelete({ userId: userId, projectId: projectId });
         await UserProject.deleteMany({ projectId });
+        await Task.deleteMany({ projectId });
         return NextResponse.json({ message: "삭제 성공" }, { status: 200 });
       } else {
         return NextResponse.json({ error: "님 관리자 아니심;" }, { status: 401 });
