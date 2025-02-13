7dd2d9b5a9acdd1ba373210d8fbb8cb878d96fa8
sisu-01 <tesv010615@naver.com>
2025-02-13 16:58:23 +0900

    임시 간트차트

--- src/components/dashboard/GanttComponent.jsx
+++ src/components/dashboard/GanttComponent.jsx
@@ -1,14 +1,12 @@
-import { Gantt } from "wx-react-gantt";
-import { Willow } from "wx-react-gantt";
+import { Gantt, WillowDark } from "wx-react-gantt";
 import "wx-react-gantt/dist/gantt.css";
-import { useRef, useEffect } from "react";
 
 const tasks = [
   {
     id: 20,
-    text: "New Task",
-    start: new Date(2024, 5, 11),
-    end: new Date(2024, 6, 12),
+    text: "철광석 캐기",
+    start: new Date(2025, 2, 1),
+    end: new Date(2025, 2, 2),
     duration: 1,
     progress: 2,
     type: "task",
@@ -16,9 +14,9 @@ const tasks = [
   },
   {
     id: 47,
-    text: "[1] Master project",
-    start: new Date(2024, 5, 12),
-    end: new Date(2024, 7, 12),
+    text: "크아악",
+    start: new Date(2025, 2, 3),
+    end: new Date(2025, 2, 4),
     duration: 8,
     progress: 0,
     parent: 0,
@@ -26,9 +24,9 @@ const tasks = [
   },
   {
     id: 22,
-    text: "Task",
-    start: new Date(2024, 7, 11),
-    end: new Date(2024, 8, 12),
+    text: "무슨 업무?",
+    start: new Date(2025, 2, 6),
+    end: new Date(2025, 2, 7),
     duration: 8,
     progress: 0,
     parent: 47,
@@ -36,9 +34,9 @@ const tasks = [
   },
   {
     id: 21,
-    text: "New Task 2",
-    start: new Date(2024, 7, 10),
-    end: new Date(2024, 8, 12),
+    text: "철골렘 만들기",
+    start: new Date(2025, 2, 10),
+    end: new Date(2025, 2, 12),
     duration: 3,
     progress: 0,
     type: "task",
@@ -46,7 +44,7 @@ const tasks = [
   },
 ];
 
-const links = [{ id: 1, source: 20, target: 21, type: "e2e" }];
+const links = [{ id: 1, source: 20, target: 21, type: "e2s" }];
 
 const scales = [
   { unit: "month", step: 1, format: "MMMM yyy" },
@@ -55,9 +53,15 @@ const scales = [
 
 const GanttComponent = () => {
   return (
-    <Willow>
-      <Gantt tasks={tasks} links={links} scales={scales} />
-    </Willow>
+    <WillowDark>
+      <Gantt
+        tasks={tasks}
+        links={links}
+        scales={scales}
+        start={new Date(2025, 2, 1)}
+        end={new Date(2025, 2, 28)}
+      />
+    </WillowDark>
   );
 }
 
