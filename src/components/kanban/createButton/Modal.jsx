ef5b52684620738b639cda40778900c262ad8e0b
sisu-01 <tesv010615@naver.com>
2025-02-20 16:01:47 +0900

    task modal css

--- src/components/kanban/createButton/Modal.jsx
+++ src/components/kanban/createButton/Modal.jsx
@@ -1,6 +1,13 @@
 import { KanbanContext } from "@/context/KanbanContext";
 import Image from "next/image";
 import { useContext, useEffect, useState } from "react";
+import styles from "./modal.module.css";
+
+const label = {
+  "todo": "진행 예정",
+  "process": "진행 중",
+  "done": "진행 완료"
+}
 
 const Modal = (props) => {
   const { columnId, projectId, closeModal } = props;
@@ -80,41 +87,54 @@ const Modal = (props) => {
   if (loading) return (<div>...Loading</div>)
 
   return (
-    <div>
-      <div>
-        task 생성
+    <div className="item-wrapper">
+      <div className="item-label">
+        {label[columnId]} task 생성
       </div>
-      <div>
-        title: <input
-          type="text"
-          value={taskTitle}
-          onChange={(e) => setTaskTitle(e.target.value)}
-          id="projectTitle"
-        />
+      <div className={`item-container ${styles.wrapper}`}>
+        <div>
+          <label for="kanbanTitle" className="form-label">제목</label>
+          <input
+            type="text"
+            className="form-control"
+            value={taskTitle}
+            onChange={(e) => setTaskTitle(e.target.value)}
+            id="kanbanTitle"
+          />
+        </div>
+        <div>
+          <label for="kanbanDate" className="form-label">task 기간</label>
+          <div className={styles.date}>
+            <input type="date" id="kanbanDate" className="form-control" defaultValue={startAt} onChange={startHandler} />~
+            <input type="date" className="form-control" defaultValue={endAt} onChange={endHandler} />
+          </div>
+        </div>
+        <div>
+          <label className="form-label">담당자</label>
+          <div className={styles.userContainer}>
+            {users.map((user) => (
+              <label key={user._id} className={styles.userWrapper}>
+                <div className={styles.user}>
+                  <Image src={user.image} width={30} height={30} alt={user.name} />
+                  {user.name}
+                </div>
+                <input
+                  type="radio"
+                  name="user"
+                  value={user._id}
+                  checked={selected === user._id}
+                  onChange={(e) => setSelected(e.target.value)}
+                />
+              </label>
+            ))}
+          </div>
+        </div>
+        <div className={styles.btnWrapper}>
+          <button onClick={handleCreateTask}>저장</button>
+          <button onClick={closeModal}>취소</button>
+        </div>
       </div>
-      <div>
-        {users.map((user) => (
-          <label key={user._id}>
-            <input
-              type="radio"
-              name="user"
-              value={user._id}
-              checked={selected === user._id}
-              onChange={(e) => setSelected(e.target.value)}
-            />
-            <Image src={user.image} width={30} height={30} alt={user.name} />
-            {user.name}
-          </label>
-        ))}
-      </div>
-      <div>
-        <input type="date" defaultValue={startAt} onChange={startHandler} />~
-        <input type="date" defaultValue={endAt} onChange={endHandler} />
-      </div>
-      <div>
-        <button onClick={handleCreateTask}>저장</button>
-        <button onClick={closeModal}>취소</button>
-      </div>
+      
     </div>
   );
 }
