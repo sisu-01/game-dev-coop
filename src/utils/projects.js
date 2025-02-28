41afec0efa74dcc367cb9becfa38be5bb2777486
sisu-01 <tesv010615@naver.com>
2025-02-28 16:51:40 +0900

    ㅇㅇ

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,7 +2,7 @@
 
 export const checkUserProjectAccess = async (email, projectId) => {
   try {
-    const response = await fetch(`http://localhost:3000/api/project/auth?email=${email}`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/project/auth?email=${email}`, {
       method: "GET",
     });
     const data = await response.json();
