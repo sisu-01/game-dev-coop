91629af758e356ae850d778907e7ed4af7e4edad
sisu-01 <tesv010615@naver.com>
2025-02-28 16:39:28 +0900

    tq

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,13 +5,11 @@ const secret = process.env.AUTH_SECRET;
 // 유저 정보를 토큰을 통해 가져오는 함수 (예시)
 export const getEmailFromToken = async (req) => {
   try {
-    const token = await getToken({ req, secret });
-    console.log(token);
+    const token = await getToken({ req, secret, secureCookie: true });
     const { email } = token;
     return email;
   } catch (error) {
     console.error(error);
     return false;
   }
-  
 }
