610816817ae0d924367360b4eb33bc7239ab91f3
sisu-01 <tesv010615@naver.com>
2025-03-03 18:02:10 +0900

    야매 shadow

--- src/components/dashboard/CustomGantt.jsx
+++ src/components/dashboard/CustomGantt.jsx
@@ -99,21 +99,43 @@ const CustomGantt = (props) => {
         <div className={styles.year}>{2025}년</div>
         <div className={styles.usersWrapper}>
           <div className={styles.users}>
-            {users.map((user) => (
-              <div key={user._id} className={styles.tempRow}>
-                <div className={styles.card}>
-                  {/* 앞면 */}
-                  <div className={styles.front}>
-                    <Image src={user.image} width={30} height={30} style={{borderRadius: "15px"}} alt={user.name} />
-                    <div>{user.name}</div>
-                  </div>
-                  {/* 뒷면 */}
-                  <div className={styles.back} style={{backgroundColor: JOBS_INT[Math.floor(user.job/1000)]}}>
-                    {JOBS_LONG_TITLE[user.job]}
+            {users.map((user, index) => {
+              // 같은 job을 가진 사용자들만 필터링
+              const sameJobUsers = users.filter(u => Math.floor(u.job / 1000) === Math.floor(user.job / 1000));
+              // 현재 job 그룹에서 첫 번째와 마지막 사용자 찾기
+              let isFirst, isLast, isMiddle, isSingle;
+              if (sameJobUsers.length === 1) {
+                isFirst = false;
+                isLast = false;
+                isMiddle = false;
+                isSingle = true;
+              } else {
+                isFirst = sameJobUsers[0]._id === user._id;
+                isLast = sameJobUsers[sameJobUsers.length - 1]._id === user._id;
+                isMiddle = !isFirst && !isLast;
+                isSingle = false;
+              }
+
+              return (
+                <div key={user._id} className={styles.tempRow}>
+                  <div
+                    className={`${styles.jobLine} ${isSingle ? styles.singleJob : ''} ${isFirst ? styles.firstJob : ''} ${isMiddle ? styles.middleJob : ''} ${isLast ? styles.lastJob : ''}`}
+                    style={{ backgroundColor: JOBS_INT[Math.floor(user.job / 1000)] }}
+                  ></div>
+                  <div className={styles.card}>
+                    {/* 앞면 */}
+                    <div className={styles.front}>
+                      <Image src={user.image} width={30} height={30} style={{ borderRadius: "15px" }} alt={user.name} />
+                      <div>{user.name}</div>
+                    </div>
+                    {/* 뒷면 */}
+                    <div className={styles.back} style={{ backgroundColor: JOBS_INT[Math.floor(user.job / 1000)] }}>
+                      {JOBS_LONG_TITLE[user.job]}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </div>
       </div>
